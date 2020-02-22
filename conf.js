daacbec0d1d4bb582cd2910f8d3e2cc82ceb7f78
angelaanaya2309 <angela.anaya.castaneda@gmail.com>
2020-02-22 12:50:36 -0500

    archivos de configuracion

---
+++ conf.js
@@ -0,0 +1,9 @@
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    allScriptsTimeout: 11000,
+    specs: ['spec.js'],
+    directConnect : true,
+    capabilities :{
+        browserName: 'chrome'
+    }
+  };
---
+++ spec.js
@@ -0,0 +1,7 @@
+describe('first test in protractor', function() {
+    it('should verify title', function() {
+      browser.get('https://angularjs.org');
+  
+      expect(browser.getTitle()).toEqual('AngularJS — Superheroic JavaScript MVW Framework');
+    });
+  });
